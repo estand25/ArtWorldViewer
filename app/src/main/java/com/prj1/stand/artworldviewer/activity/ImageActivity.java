f77ec3ca1a90ecfecd9098c38f60250abb96143f
Standley Eugene <estand25@gmail.com>
2017-06-19 20:42:50 -0400

    Update App - Adding website view in Gallery+ (working on) - Updated Gallery to show artwork relative to there pix size like in Gallery+ view (working on) - Adding moving group artwork page functionality (working on)

--- app/src/main/java/com/prj1/stand/artworldviewer/activity/ImageActivity.java
+++ app/src/main/java/com/prj1/stand/artworldviewer/activity/ImageActivity.java
@@ -1,11 +1,15 @@
 package com.prj1.stand.artworldviewer.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.prj1.stand.artworldviewer.R;
 
 public class ImageActivity extends AppCompatActivity {
 	public static final String EXTRA_IMAGE = "image";
+	ImageDetailFragment imageDetailFragment;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,16 @@ public class ImageActivity extends AppCompatActivity {
 		// Pass the ArtworkCard to the fragment
 		//savedInstanceState.putParcelable(ImageDetailFragment.EXTRA_IMAGE_DETAIL,getIntent().getParcelableExtra(EXTRA_IMAGE));
 		
-		ImageDetailFragment imageDetailFragment = ((ImageDetailFragment) getSupportFragmentManager()
-			.findFragmentById(R.id.fragmentDetail));
+		//ImageDetailFragment imageDetailFragment = ((ImageDetailFragment) getSupportFragmentManager()
+		//	.findFragmentById(R.id.fragmentDetail));
+		
+		if(savedInstanceState == null)
+		{
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+			
+			ImageDetailFragment imageDetailFragment = new ImageDetailFragment();
+			fragmentTransaction.add(R.)
+		}
 	}
 }
