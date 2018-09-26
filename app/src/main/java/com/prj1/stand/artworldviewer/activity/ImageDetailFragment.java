a6cba2a144762f35371b704755c963e0330c805b
Standley Eugene <estand25@gmail.com>
2018-09-26 17:01:15 -0400

    Both Gallery and Gallery+ both are appearing correctly.

--- app/src/main/java/com/prj1/stand/artworldviewer/activity/ImageDetailFragment.java
+++ app/src/main/java/com/prj1/stand/artworldviewer/activity/ImageDetailFragment.java
@@ -2,6 +2,7 @@ package com.prj1.stand.artworldviewer.activity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -9,6 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.androidquery.AQuery;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
@@ -26,10 +31,12 @@ import java.util.List;
 
 public class ImageDetailFragment extends Fragment {
 	public static final String EXTRA_IMAGE_DETAIL = "image_detail";
-	
-	RecyclerView imageRecycleView;
-	SlimAdapter imageSlimAdpater;
-	TouchImageView touchImageView;
+
+	ImageView imageView;
+	TextView artWorkTitle;
+	TextView artCollInstit;
+	TextView artAddInform;
+	ImageButton closeButton;
 
 	public ImageDetailFragment() {
 	}
@@ -52,11 +59,28 @@ public class ImageDetailFragment extends Fragment {
 		Log.v("ImageDetailFragment", "onCreateView");
 		
 		// Inflate the layout for this fragment
-		View rootView =  inflater.inflate(R.layout.fragment_image_detail, container, false);
-		
-		imageRecycleView = (RecyclerView) rootView.findViewById(R.id.image_recycler_view);
+		final View rootView =  inflater.inflate(R.layout.item_image_details, container, false);
+
+        artWorkTitle = (TextView) rootView.findViewById(R.id.image_title);
+        artCollInstit = (TextView) rootView.findViewById(R.id.image_collection_Institution);
+        artAddInform = (TextView) rootView.findViewById(R.id.image_additional_information);
+        closeButton = (ImageButton) rootView.findViewById(R.id.btnImage_close);
 
 		ArtworkCard artworkCard = this.getArguments().getParcelable(EXTRA_IMAGE_DETAIL);
+
+		AQuery aq  = new AQuery(rootView);
+		aq.id(R.id.pic_image).image(artworkCard.getAc_thumbnail()).visible();
+
+		artWorkTitle.setText(artworkCard.getAc_title());
+		artCollInstit.setText(artworkCard.getAc_collective_institution());
+		artAddInform.setText(artworkCard.getAc_additional_information());
+
+		closeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getActivity().finish();
+			}
+		});
 
 		return rootView;
 	}
